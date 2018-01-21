4d9194435dfa9c29b4e7aa665ec44bd8c3697576
Ying Li <ying@iglassus.com>
2018-01-21 15:58:47 -0800

    Adjust status bar, etc. But horizontal does not work well.

--- sample/src/main/java/com/daasuu/exoplayerfilter/MainActivity.java
+++ sample/src/main/java/com/daasuu/exoplayerfilter/MainActivity.java
@@ -66,11 +66,16 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         setUpViews();
 
+        // https://developer.android.com/training/system-ui/immersive.html
         // Hide the status bar on Android 4.1 (API level 16) and higher:
+        // https://bradmartin.net/2016/03/10/fullscreen-and-navigation-bar-color-in-a-nativescript-android-app/
         View decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        // int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN; // show status bar
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;  // status bar now shown when the app starts. But once it is pulled down, it will stay there
+        int uiOptions =  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                 | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
 //        // Remember that you should never show the action bar if the
 //        // status bar is hidden, so hide that too if necessary.
